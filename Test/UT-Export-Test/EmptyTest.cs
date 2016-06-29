d2170c1e2b673e04bb002e3e7c4ebb91ad2eea91
yongguang <ygma@thoughtworks.com>
2016-06-30 06:23:20 +0800

    Create empty UTExport and test projects

---
+++ Test/UT-Export-Test/EmptyTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace UTExport.Test
+{
+    public class EmptyTest
+    {
+        [Fact]
+        public void FactMethodName()
+        {
+            Assert.True(true);
+        }
+    }
+}
