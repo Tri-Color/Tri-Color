e7b339f504d9cc807ec93d2091042704b1387c2a
yongguang <ygma@thoughtworks.com>
2016-07-04 00:11:14 +0800

    Add test to check space and tab

---
+++ Test/UT-Export-Test/JT/Fixtures/space-and-tab-spec.es6
@@ -0,0 +1,8 @@
+describe("top describe", function () {
+    'use strict'
+
+	describe("describe with 3 spaces and 1 tab", function () {
+		describe("describe with 5 spaces and 1 tab", function () {
+        });
+    });
+});
---
+++ Test/UT-Export-Test/JT/when_export_jt_with_space_and_tab.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using UTExport.JT;
+using Xunit;
+
+namespace UTExport.Test.JT
+{
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public class when_export_jt_with_space_and_tab
+    {
+        [Fact]
+        public void should_export_jt_with_correct_levels()
+        {
+            var fixtureFileFullName = Utils.GetFixtureFileFullName("JT\\Fixtures\\space-and-tab-spec.es6");
+
+            var jtManager = new JTManager();
+            List<UTInfo> utInfos = jtManager.Export(fixtureFileFullName);
+
+            Assert.Equal(1, utInfos.Count);
+
+            UTInfo topUtInfo = utInfos.Single();
+            Assert.Equal(1, topUtInfo.Children.Count);
+            
+            UTInfo child = topUtInfo.Children.Single();
+            Assert.Equal("describe with 3 spaces and 1 tab", child.Description);
+            Assert.Equal(1, child.Children.Count);
+            
+            UTInfo grandson = child.Children.Single();
+            Assert.Equal("describe with 5 spaces and 1 tab", grandson.Description);
+        }
+    }
+}
